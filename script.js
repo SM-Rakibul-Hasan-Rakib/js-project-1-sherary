195617ad08f3e3ca6d449d65c1ece15f49892826
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-01-12 20:05:04 +0600

    project done

--- script.js
+++ script.js
@@ -6,7 +6,30 @@ rect.addEventListener("mousemove", function (details) {
 
   if (insiderectval < rectanglelocation.width / 2) {
     console.log("left");
+    // Map the value from a range of 0 to 250 to a range of 255 to 0
+    var redcolor = gsap.utils.mapRange(
+      0,
+      rectanglelocation.width / 2,
+      255,
+      0,
+      insiderectval
+    );
+
+    gsap.to(rect, {
+      backgroundColor: `rgb(${redcolor}, 0, 0)`,
+      duration: 0.3,
+    });
   } else {
     console.log("right");
+    gsap.to(rect, {
+      backgroundColor: `rgb(0, 0, 255)`,
+      duration: 0.3,
+    });
   }
+});
+
+rect.addEventListener("mouseleaves", function () {
+  gsap.to(rect, {
+    backgroundColor: "white",
+  });
 });
