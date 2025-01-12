34ea6b4dd27644c54bf41194b6f89bff1613d3fa
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-01-12 19:34:51 +0600

    3rd commit

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var rect = document.querySelector("#center");
 
-rect.addEventListener("mousemove", function () {
-  console.log(rect.getBoundingClientRect());
+rect.addEventListener("mousemove", function (details) {
+  var rectanglelocation = rect.getBoundingClientRect();
+  var insiderectval = details.clientX - rectanglelocation.left;
+
+  if (insiderectval < rectanglelocation.width / 2) {
+    console.log("left");
+  } else {
+    console.log("right");
+  }
 });
